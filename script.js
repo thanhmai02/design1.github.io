6d0856425a72cc1564fc318d4ce5f02e119ede57
ThanhMai <mai.ltt.62cntt@ntu.edu.vn>
2022-06-30 07:40:25 +0700

    design 1

---
+++ script.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    $('.img-group').slick({
+        slidesToShow: 4,
+        infinite: true,
+        dots: true,
+        arrows:false,
+    });
+});
+$(document).ready(function(){
+    $('.row-6').slick({
+        slidesToShow: 2,
+        infinite: true,
+        dots: true,
+        arrows:false,
+     });
+});
+$(document).ready(function(){
+    $('.six-img').slick({
+        slidesToShow: 3,
+        infinite: false,
+        dots: true,
+        arrows:false,
+     });
+});
+$(document).ready(function(){
+    $('.se-img').slick({
+        slidesToShow: 2.5,
+        infinite: false,
+        dots: true,
+        arrows:false,
+     });
+});
